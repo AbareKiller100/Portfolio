abcad742ba114c1a0f89888a2bb920ca73fe5b74
AbareKiller100 <themr.camper213@gmail.com>
2024-09-18 19:36:35 -0300

    avances

--- my-vite-app/src/Views/About/Skills.jsx
+++ my-vite-app/src/Views/About/Skills.jsx
@@ -5,32 +5,77 @@ import CSS from '../../../Capturas_Proyectos/css3-original.svg'
 import TS from '../../../Capturas_Proyectos/typescript-original.svg'
 import SQL from '../../../Capturas_Proyectos/SQL-icon.png'
 
+import NODE from '../../../Capturas_Proyectos/nodejs-original-wordmark.svg'
+import EXPRESS from '../../../Capturas_Proyectos/express-original-wordmark.svg'
+import SEQUELIZE from '../../../Capturas_Proyectos/sequelize-original.svg'
+import REACT from '../../../Capturas_Proyectos/react-original.svg'
+import REDUX from '../../../Capturas_Proyectos/redux-original.svg'
+
+import POSTGRE from '../../../Capturas_Proyectos/postgresql-original-wordmark.svg'
+import APPSHEET from '../../../Capturas_Proyectos/appsheet-icon.png'
+import GOOGLESHEETS from '../../../Capturas_Proyectos/google_sheets-icon.jpg'
+
+import GIT from '../../../Capturas_Proyectos/git-plain.svg'
+import GITHUB from '../../../Capturas_Proyectos/github-original.svg'
+
+import VS from '../../../Capturas_Proyectos/vscode-original.svg'
+import POSTMAN from '../../../Capturas_Proyectos/postman-original-wordmark.svg'
 
 const Skills= () =>{
     return(
         <div className={style.skills}>
-          <h2>Skills</h2>
-          <h3>Tech skills</h3>
+          <h2>Tech skills</h2>
+          <div className={style.prim}>
+
+          <div className={style.lenguajes}>
           <h3>Lenguajes de Programación</h3>
           <img src={JS} alt="JavaScript" />
-          <img src={HTML} alt="HTML" />
-          <img src={CSS} alt="CSS" />
           <img src={SQL} alt="SQL" />
           <img src={TS} alt='TS' />
-          <h3>Frameworks</h3>
-            <ul>
-                <li>NodeJS</li>
-                <li>ExpressJS</li>
-                <li>Sequelize</li>
-                <li>ReactJS</li>
-                <li>Redux</li>
-            </ul>
-            <h3>Base de datos</h3>
-            <ul>
-                <li>PostgreSQL</li>
-            </ul>
+          </div>
+
+          <div className={style.diseno}>
+            <h3>Diseño y maquetación</h3>
+            <img src={HTML} alt="HTML" />
+            <img src={CSS} alt="CSS" />
+          </div>
           
-          <h3>Soft skills</h3>
+          </div>
+          <div className={style.frameworks}>
+            <h3>Frameworks</h3>
+
+            <p>Front-End</p>
+            <img src={REACT} alt="React" />
+            <img src={REDUX} alt='Redux' />
+
+            <p>Back-End</p>
+            <img src={NODE} alt="Node" />
+            <img src={EXPRESS} alt="Express" />
+            <img src={SEQUELIZE} alt="Sequelize" />
+          </div>
+          <div className={style.database}>
+            <h3>Base de datos</h3>
+            <img src={POSTGRE} alt="PostgreSQL" />
+            <img src={GOOGLESHEETS} alt="Google Sheets" />
+          </div>
+          <div className={style.platform}>
+            <h3>Plataformas de desarrollo</h3>
+            <img src={APPSHEET} alt="AppSheet" />
+          </div>
+          <div className={style.tools}>
+            <h3>Herramientas de desarrollo</h3>
+            <img src={VS} alt="VS Code" />
+            <img src={POSTMAN} alt="Postman" />
+          </div>
+          <div className={style.version}>
+            <h3>Control de versiones</h3>
+            <img src={GIT} alt="GIT" />
+            <img src={GITHUB} alt="GitHub" />
+          </div>
+
+          
+          
+          <h2>Soft skills</h2>
           <ul className={style.soft}>
             <li>Análisis de problemas</li>
             <li>Aprendizaje ágil</li>
